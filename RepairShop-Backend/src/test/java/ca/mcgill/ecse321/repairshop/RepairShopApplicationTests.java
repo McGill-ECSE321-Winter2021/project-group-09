76e5f24df3989bf2243ab2612ab8fa7ebaab1da1
Jacek Dziewonski <jacek@live.ca>
2021-02-11 00:06:25 -0800

    Started the spring boot project

---
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/RepairShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.repairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
