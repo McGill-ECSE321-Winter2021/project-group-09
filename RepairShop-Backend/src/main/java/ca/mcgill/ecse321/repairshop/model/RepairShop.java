917ee014b4bc6657531f70e139701378c27d19fa
Jacek Dziewonski <jacek@live.ca>
2021-02-15 19:02:16 -0800

    Added jpa annotations to User classes + repair shop

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/model/RepairShop.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/model/RepairShop.java
@@ -1,403 +1,77 @@
 package ca.mcgill.ecse321.repairshop.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.persistence.*;
 import java.util.List;
 
-// line 2 "model.ump"
-// line 141 "model.ump"
+@Entity
 public class RepairShop {
 
-    //------------------------
-    // MEMBER VARIABLES
-    //------------------------
 
-    //RepairShop Attributes
     private Long repairShopID;
 
-    //RepairShop Associations
-    private Business business;
-    private final List<Customer> customers;
-    private final List<Technician> technicians;
-    private final List<Admin> admin;
-
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
-
-    public RepairShop(Long aRepairShopID) {
-        repairShopID = aRepairShopID;
-        customers = new ArrayList<Customer>();
-        technicians = new ArrayList<Technician>();
-        admin = new ArrayList<Admin>();
-    }
-
-    //------------------------
-    // INTERFACE
-    //------------------------
-
-    /* Code from template association_MinimumNumberOfMethod */
-    public static int minimumNumberOfCustomers() {
-        return 0;
-    }
-
-    /* Code from template association_MinimumNumberOfMethod */
-    public static int minimumNumberOfTechnicians() {
-        return 0;
-    }
-
-    /* Code from template association_MinimumNumberOfMethod */
-    public static int minimumNumberOfAdmin() {
-        return 0;
-    }
-
-    public boolean setRepairShopID(Long aRepairShopID) {
-        boolean wasSet = false;
-        repairShopID = aRepairShopID;
-        wasSet = true;
-        return wasSet;
-    }
-
+    @Id
+    @GeneratedValue
     public Long getRepairShopID() {
         return repairShopID;
     }
 
-    /* Code from template association_GetOne */
+    public void setRepairShopID(Long aRepairShopID) {
+        repairShopID = aRepairShopID;
+    }
+
+    ////////////////////////////////
+
+    private Business business;
+
+    @OneToOne(cascade = CascadeType.ALL)
     public Business getBusiness() {
         return business;
     }
 
-    public boolean hasBusiness() {
-        boolean has = business != null;
-        return has;
-    }
-
-    /* Code from template association_GetMany */
-    public Customer getCustomer(int index) {
-        Customer aCustomer = customers.get(index);
-        return aCustomer;
-    }
-
-    public List<Customer> getCustomers() {
-        List<Customer> newCustomers = Collections.unmodifiableList(customers);
-        return newCustomers;
-    }
-
-    public int numberOfCustomers() {
-        int number = customers.size();
-        return number;
-    }
-
-    public boolean hasCustomers() {
-        boolean has = customers.size() > 0;
-        return has;
-    }
-
-    public int indexOfCustomer(Customer aCustomer) {
-        int index = customers.indexOf(aCustomer);
-        return index;
-    }
-
-    /* Code from template association_GetMany */
-    public Technician getTechnician(int index) {
-        Technician aTechnician = technicians.get(index);
-        return aTechnician;
-    }
-
-    public List<Technician> getTechnicians() {
-        List<Technician> newTechnicians = Collections.unmodifiableList(technicians);
-        return newTechnicians;
-    }
-
-    public int numberOfTechnicians() {
-        int number = technicians.size();
-        return number;
-    }
-
-    public boolean hasTechnicians() {
-        boolean has = technicians.size() > 0;
-        return has;
-    }
-
-    public int indexOfTechnician(Technician aTechnician) {
-        int index = technicians.indexOf(aTechnician);
-        return index;
-    }
-
-    /* Code from template association_GetMany */
-    public Admin getAdmin(int index) {
-        Admin aAdmin = admin.get(index);
-        return aAdmin;
-    }
-
-    public List<Admin> getAdmin() {
-        List<Admin> newAdmin = Collections.unmodifiableList(admin);
-        return newAdmin;
-    }
-
-    public int numberOfAdmin() {
-        int number = admin.size();
-        return number;
-    }
-
-    public boolean hasAdmin() {
-        boolean has = admin.size() > 0;
-        return has;
-    }
-
-    public int indexOfAdmin(Admin aAdmin) {
-        int index = admin.indexOf(aAdmin);
-        return index;
-    }
-
-    /* Code from template association_SetOptionalOneToOne */
-    public boolean setBusiness(Business aNewBusiness) {
-        boolean wasSet = false;
-        if (business != null && !business.equals(aNewBusiness) && equals(business.getRepairShop())) {
-            //Unable to setBusiness, as existing business would become an orphan
-            return wasSet;
-        }
-
+    public void setBusiness(Business aNewBusiness) {
         business = aNewBusiness;
-        RepairShop anOldRepairShop = aNewBusiness != null ? aNewBusiness.getRepairShop() : null;
-
-        if (!this.equals(anOldRepairShop)) {
-            if (anOldRepairShop != null) {
-                anOldRepairShop.business = null;
-            }
-            if (business != null) {
-                business.setRepairShop(this);
-            }
-        }
-        wasSet = true;
-        return wasSet;
     }
 
-    /* Code from template association_AddManyToOne */
-    public Customer addCustomer(String aEmail, String aPassword, String aPhoneNumber, String aName, String aAddress, Long aCustomerID) {
-        return new Customer(aEmail, aPassword, aPhoneNumber, aName, aAddress, aCustomerID, this);
+    ////////////////////////////////
+
+    private List<Customer> customers;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<Customer> getCustomers() {
+        return customers;
     }
 
-    public boolean addCustomer(Customer aCustomer) {
-        boolean wasAdded = false;
-        if (customers.contains(aCustomer)) {
-            return false;
-        }
-        RepairShop existingRepairShop = aCustomer.getRepairShop();
-        boolean isNewRepairShop = existingRepairShop != null && !this.equals(existingRepairShop);
-        if (isNewRepairShop) {
-            aCustomer.setRepairShop(this);
-        } else {
-            customers.add(aCustomer);
-        }
-        wasAdded = true;
-        return wasAdded;
+    public void setCustomers(List<Customer> newCustomers) {
+        customers = newCustomers;
     }
 
-    public boolean removeCustomer(Customer aCustomer) {
-        boolean wasRemoved = false;
-        //Unable to remove aCustomer, as it must always have a repairShop
-        if (!this.equals(aCustomer.getRepairShop())) {
-            customers.remove(aCustomer);
-            wasRemoved = true;
-        }
-        return wasRemoved;
+    ////////////////////////////////
+
+    private List<Technician> technicians;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<Technician> getTechnicians() {
+        return technicians;
     }
 
-    /* Code from template association_AddIndexControlFunctions */
-    public boolean addCustomerAt(Customer aCustomer, int index) {
-        boolean wasAdded = false;
-        if (addCustomer(aCustomer)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfCustomers()) {
-                index = numberOfCustomers() - 1;
-            }
-            customers.remove(aCustomer);
-            customers.add(index, aCustomer);
-            wasAdded = true;
-        }
-        return wasAdded;
+    public void setTechnicians(List<Technician> technicians) {
+        this.technicians = technicians;
     }
 
-    public boolean addOrMoveCustomerAt(Customer aCustomer, int index) {
-        boolean wasAdded = false;
-        if (customers.contains(aCustomer)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfCustomers()) {
-                index = numberOfCustomers() - 1;
-            }
-            customers.remove(aCustomer);
-            customers.add(index, aCustomer);
-            wasAdded = true;
-        } else {
-            wasAdded = addCustomerAt(aCustomer, index);
-        }
-        return wasAdded;
+    ////////////////////////////////
+
+    private List<Admin> admin;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<Admin> getAdmin() {
+        return admin;
     }
 
-    /* Code from template association_AddManyToOne */
-    public Technician addTechnician(String aEmail, String aPassword, String aPhoneNumber, String aName, String aAddress, Long aTechnicianID) {
-        return new Technician(aEmail, aPassword, aPhoneNumber, aName, aAddress, aTechnicianID, this);
+    public void setAdmin(List<Admin> admin) {
+        this.admin = admin;
     }
 
-    public boolean addTechnician(Technician aTechnician) {
-        boolean wasAdded = false;
-        if (technicians.contains(aTechnician)) {
-            return false;
-        }
-        RepairShop existingRepairShop = aTechnician.getRepairShop();
-        boolean isNewRepairShop = existingRepairShop != null && !this.equals(existingRepairShop);
-        if (isNewRepairShop) {
-            aTechnician.setRepairShop(this);
-        } else {
-            technicians.add(aTechnician);
-        }
-        wasAdded = true;
-        return wasAdded;
-    }
-
-    public boolean removeTechnician(Technician aTechnician) {
-        boolean wasRemoved = false;
-        //Unable to remove aTechnician, as it must always have a repairShop
-        if (!this.equals(aTechnician.getRepairShop())) {
-            technicians.remove(aTechnician);
-            wasRemoved = true;
-        }
-        return wasRemoved;
-    }
-
-    /* Code from template association_AddIndexControlFunctions */
-    public boolean addTechnicianAt(Technician aTechnician, int index) {
-        boolean wasAdded = false;
-        if (addTechnician(aTechnician)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfTechnicians()) {
-                index = numberOfTechnicians() - 1;
-            }
-            technicians.remove(aTechnician);
-            technicians.add(index, aTechnician);
-            wasAdded = true;
-        }
-        return wasAdded;
-    }
-
-    public boolean addOrMoveTechnicianAt(Technician aTechnician, int index) {
-        boolean wasAdded = false;
-        if (technicians.contains(aTechnician)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfTechnicians()) {
-                index = numberOfTechnicians() - 1;
-            }
-            technicians.remove(aTechnician);
-            technicians.add(index, aTechnician);
-            wasAdded = true;
-        } else {
-            wasAdded = addTechnicianAt(aTechnician, index);
-        }
-        return wasAdded;
-    }
-
-    /* Code from template association_AddManyToOne */
-    public Admin addAdmin(String aEmail, String aPassword, String aPhoneNumber, String aName, String aAddress, Long aAdminID) {
-        return new Admin(aEmail, aPassword, aPhoneNumber, aName, aAddress, aAdminID, this);
-    }
-
-    public boolean addAdmin(Admin aAdmin) {
-        boolean wasAdded = false;
-        if (admin.contains(aAdmin)) {
-            return false;
-        }
-        RepairShop existingRepairShop = aAdmin.getRepairShop();
-        boolean isNewRepairShop = existingRepairShop != null && !this.equals(existingRepairShop);
-        if (isNewRepairShop) {
-            aAdmin.setRepairShop(this);
-        } else {
-            admin.add(aAdmin);
-        }
-        wasAdded = true;
-        return wasAdded;
-    }
-
-    public boolean removeAdmin(Admin aAdmin) {
-        boolean wasRemoved = false;
-        //Unable to remove aAdmin, as it must always have a repairShop
-        if (!this.equals(aAdmin.getRepairShop())) {
-            admin.remove(aAdmin);
-            wasRemoved = true;
-        }
-        return wasRemoved;
-    }
-
-    /* Code from template association_AddIndexControlFunctions */
-    public boolean addAdminAt(Admin aAdmin, int index) {
-        boolean wasAdded = false;
-        if (addAdmin(aAdmin)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfAdmin()) {
-                index = numberOfAdmin() - 1;
-            }
-            admin.remove(aAdmin);
-            admin.add(index, aAdmin);
-            wasAdded = true;
-        }
-        return wasAdded;
-    }
-
-    public boolean addOrMoveAdminAt(Admin aAdmin, int index) {
-        boolean wasAdded = false;
-        if (admin.contains(aAdmin)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfAdmin()) {
-                index = numberOfAdmin() - 1;
-            }
-            admin.remove(aAdmin);
-            admin.add(index, aAdmin);
-            wasAdded = true;
-        } else {
-            wasAdded = addAdminAt(aAdmin, index);
-        }
-        return wasAdded;
-    }
-
-    public void delete() {
-        Business existingBusiness = business;
-        business = null;
-        if (existingBusiness != null) {
-            existingBusiness.delete();
-            existingBusiness.setRepairShop(null);
-        }
-        while (customers.size() > 0) {
-            Customer aCustomer = customers.get(customers.size() - 1);
-            aCustomer.delete();
-            customers.remove(aCustomer);
-        }
-
-        while (technicians.size() > 0) {
-            Technician aTechnician = technicians.get(technicians.size() - 1);
-            aTechnician.delete();
-            technicians.remove(aTechnician);
-        }
-
-        while (admin.size() > 0) {
-            Admin aAdmin = admin.get(admin.size() - 1);
-            aAdmin.delete();
-            admin.remove(aAdmin);
-        }
-
-    }
-
+    ////////////////////////////////
 
     public String toString() {
         return super.toString() + "[" +
--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/model/Technician.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/model/Technician.java
@@ -1,189 +1,57 @@
 package ca.mcgill.ecse321.repairshop.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.persistence.*;
 import java.util.List;
 
-// line 17 "model.ump"
-// line 92 "model.ump"
+@Entity
 public class Technician extends User {
 
-    //------------------------
-    // MEMBER VARIABLES
-    //------------------------
-
-    //Technician Attributes
     private Long technicianID;
 
-    //Technician Associations
-    private final List<TimeSlot> timeslots;
-    private RepairShop repairShop;
-
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
-
-    public Technician(String aEmail, String aPassword, String aPhoneNumber, String aName, String aAddress, Long aTechnicianID, RepairShop aRepairShop) {
-        super(aEmail, aPassword, aPhoneNumber, aName, aAddress);
-        technicianID = aTechnicianID;
-        timeslots = new ArrayList<TimeSlot>();
-        boolean didAddRepairShop = setRepairShop(aRepairShop);
-        if (!didAddRepairShop) {
-            throw new RuntimeException("Unable to create technician due to repairShop. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-        }
-    }
-
-    //------------------------
-    // INTERFACE
-    //------------------------
-
-    /* Code from template association_MinimumNumberOfMethod */
-    public static int minimumNumberOfTimeslots() {
-        return 0;
-    }
-
-    public boolean setTechnicianID(Long aTechnicianID) {
-        boolean wasSet = false;
-        technicianID = aTechnicianID;
-        wasSet = true;
-        return wasSet;
-    }
-
+    @Id
+    @GeneratedValue
     public Long getTechnicianID() {
         return technicianID;
     }
 
-    /* Code from template association_GetMany */
-    public TimeSlot getTimeslot(int index) {
-        TimeSlot aTimeslot = timeslots.get(index);
-        return aTimeslot;
+    public void setTechnicianID(Long technicianID) {
+        this.technicianID = technicianID;
     }
 
+    ////////////////////////////////
+
+    private List<TimeSlot> timeslots;
+    @OneToMany(targetEntity=TimeSlot.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "technician")
     public List<TimeSlot> getTimeslots() {
-        List<TimeSlot> newTimeslots = Collections.unmodifiableList(timeslots);
-        return newTimeslots;
+        return timeslots;
     }
 
-    public int numberOfTimeslots() {
-        int number = timeslots.size();
-        return number;
+    public void setTimeslots(List<TimeSlot> timeslots) {
+        this.timeslots = timeslots;
     }
 
-    public boolean hasTimeslots() {
-        boolean has = timeslots.size() > 0;
-        return has;
-    }
+    ////////////////////////////////
 
-    public int indexOfTimeslot(TimeSlot aTimeslot) {
-        int index = timeslots.indexOf(aTimeslot);
-        return index;
-    }
+    private RepairShop repairShop;
 
-    /* Code from template association_GetOne */
     public RepairShop getRepairShop() {
         return repairShop;
     }
 
-    /* Code from template association_AddManyToOne */
-    public TimeSlot addTimeslot(Long aTimeSlotID, String aStartDateTime, String aEndDateTime) {
-        return new TimeSlot(aTimeSlotID, aStartDateTime, aEndDateTime, this);
+    public void setRepairShop(RepairShop repairShop) {
+        this.repairShop = repairShop;
     }
 
-    public boolean addTimeslot(TimeSlot aTimeslot) {
-        boolean wasAdded = false;
-        if (timeslots.contains(aTimeslot)) {
-            return false;
-        }
-        Technician existingTechnician = aTimeslot.getTechnician();
-        boolean isNewTechnician = existingTechnician != null && !this.equals(existingTechnician);
-        if (isNewTechnician) {
-            aTimeslot.setTechnician(this);
-        } else {
-            timeslots.add(aTimeslot);
-        }
-        wasAdded = true;
-        return wasAdded;
-    }
-
-    public boolean removeTimeslot(TimeSlot aTimeslot) {
-        boolean wasRemoved = false;
-        //Unable to remove aTimeslot, as it must always have a technician
-        if (!this.equals(aTimeslot.getTechnician())) {
-            timeslots.remove(aTimeslot);
-            wasRemoved = true;
-        }
-        return wasRemoved;
-    }
-
-    /* Code from template association_AddIndexControlFunctions */
-    public boolean addTimeslotAt(TimeSlot aTimeslot, int index) {
-        boolean wasAdded = false;
-        if (addTimeslot(aTimeslot)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfTimeslots()) {
-                index = numberOfTimeslots() - 1;
-            }
-            timeslots.remove(aTimeslot);
-            timeslots.add(index, aTimeslot);
-            wasAdded = true;
-        }
-        return wasAdded;
-    }
-
-    public boolean addOrMoveTimeslotAt(TimeSlot aTimeslot, int index) {
-        boolean wasAdded = false;
-        if (timeslots.contains(aTimeslot)) {
-            if (index < 0) {
-                index = 0;
-            }
-            if (index > numberOfTimeslots()) {
-                index = numberOfTimeslots() - 1;
-            }
-            timeslots.remove(aTimeslot);
-            timeslots.add(index, aTimeslot);
-            wasAdded = true;
-        } else {
-            wasAdded = addTimeslotAt(aTimeslot, index);
-        }
-        return wasAdded;
-    }
-
-    /* Code from template association_SetOneToMany */
-    public boolean setRepairShop(RepairShop aRepairShop) {
-        boolean wasSet = false;
-        if (aRepairShop == null) {
-            return wasSet;
-        }
-
-        RepairShop existingRepairShop = repairShop;
-        repairShop = aRepairShop;
-        if (existingRepairShop != null && !existingRepairShop.equals(aRepairShop)) {
-            existingRepairShop.removeTechnician(this);
-        }
-        repairShop.addTechnician(this);
-        wasSet = true;
-        return wasSet;
-    }
-
-    public void delete() {
-        for (int i = timeslots.size(); i > 0; i--) {
-            TimeSlot aTimeslot = timeslots.get(i - 1);
-            aTimeslot.delete();
-        }
-        RepairShop placeholderRepairShop = repairShop;
-        this.repairShop = null;
-        if (placeholderRepairShop != null) {
-            placeholderRepairShop.removeTechnician(this);
-        }
-        super.delete();
-    }
-
+    ////////////////////////////////
 
     public String toString() {
         return super.toString() + "[" +
                 "technicianID" + ":" + getTechnicianID() + "]" + System.getProperties().getProperty("line.separator") +
                 "  " + "repairShop = " + (getRepairShop() != null ? Integer.toHexString(System.identityHashCode(getRepairShop())) : "null");
     }
+
+
+
+
+
 }
