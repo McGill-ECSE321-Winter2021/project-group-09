b0a2e80b4e64d8ea85c006fda4eb361d94005819
Shaswata Bhattacharyya <shaswatabhatt2030@gmail.com>
2021-02-18 20:52:53 +0600

    Created CRUD repo for Technician

---
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dao/TechnicianRepository.java
@@ -0,0 +1,27 @@
+package ca.mcgill.ecse321.repairshop.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import ca.mcgill.ecse321.repairshop.model.Technician;
+import ca.mcgill.ecse321.repairshop.model.TimeSlot;
+
+
+
+
+public interface TechnicianRepository extends CrudRepository<Technician, Long>{
+	
+	Optional<Technician> findById(Long technicianID);
+	
+	Technician findByTimeSlot(TimeSlot timeSlot);
+	
+	List<Technician> findAll();	
+	
+	void deleteById(Long technicianID);
+	
+	void deleteAll();
+	
+}
