8636597990042acfd9e141d397e85e6870e1b11e
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-02-19 11:43:42 -0500

    Testing ServiceTests

---
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/dao/ServiceTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.repairshop.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
