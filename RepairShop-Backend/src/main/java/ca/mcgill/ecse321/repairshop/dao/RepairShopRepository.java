48bc67785611ca28cebcaa74b934ffa47b453b48
Sandy L <70775435+sandyl289@users.noreply.github.com>
2021-02-18 23:24:11 -0500

    Rename repairShopRepo to repairShopRepository for consistency

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dao/RepairShopRepository.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dao/RepairShopRepository.java
@@ -3,5 +3,5 @@ package ca.mcgill.ecse321.repairshop.dao;
 import ca.mcgill.ecse321.repairshop.model.RepairShop;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RepairShopRepo extends CrudRepository<RepairShop, Long> {
+public interface RepairShopRepository extends CrudRepository<RepairShop, Long> {
 }
