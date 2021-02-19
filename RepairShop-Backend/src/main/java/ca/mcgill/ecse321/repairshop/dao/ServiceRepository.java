f2803a20f477c178f693591dc75ebbb3d9fd7539
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-02-19 09:00:53 -0500

    Manually add CRUD repos back

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dao/ServiceRepository.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dao/ServiceRepository.java
@@ -3,8 +3,10 @@ package ca.mcgill.ecse321.repairshop.dao;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.repairshop.model.Service;
 
-public interface ServiceRepository extends CrudRepository<Service, Integer> {
+public interface ServiceRepository extends CrudRepository<Service, Long> {
 
     Service findServiceByName(String name);
+
+    Service findServiceById(Long serviceId);
 
 }
