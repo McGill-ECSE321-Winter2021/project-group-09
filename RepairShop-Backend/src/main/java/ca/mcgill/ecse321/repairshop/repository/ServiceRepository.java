20ff034779b3276b82c5cbdebab52ab33ccbb597
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-03-02 17:08:01 -0500

    Initial code for ServiceDto, ServiceService and ServiceController done

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/repository/ServiceRepository.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/repository/ServiceRepository.java
@@ -3,8 +3,12 @@ package ca.mcgill.ecse321.repairshop.repository;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.repairshop.model.Service;
 
+import java.util.List;
+
 public interface ServiceRepository extends CrudRepository<Service, String> {
 
     Service findServiceByName(String name);
+
+    List<Service> findAll();
 
 }
