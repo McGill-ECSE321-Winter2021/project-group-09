e718a1d8ef21c8a0525efde40fa1730a5b211914
Sandy L <70775435+sandyl289@users.noreply.github.com>
2021-03-04 17:10:23 -0500

    Update basic methods for Business Controller, Service
    - Changed some methods to make it similar to everyone else
    - Updated methods in BusinessService to return a DTO object like everyone else
    - Commented the controller methods
    - I'm still writing the Business Service tests

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/BusinessRestController.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/BusinessRestController.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse321.repairshop.controller;
 
 import ca.mcgill.ecse321.repairshop.dto.BusinessDto;
+import ca.mcgill.ecse321.repairshop.dto.TimeSlotDto;
 import ca.mcgill.ecse321.repairshop.model.Business;
+import ca.mcgill.ecse321.repairshop.model.TimeSlot;
 import ca.mcgill.ecse321.repairshop.repository.BusinessRepository;
 import ca.mcgill.ecse321.repairshop.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,74 +11,108 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api/business")
 public class BusinessRestController {
 
     @Autowired
     BusinessService businessService;
-    private Object HttpsStatus;
-
-    //GETTERS
-    @GetMapping(value = {"/businesses", "/businesses/"})
-    public List<BusinessDto> getAllBusinesses() {
-        return businessService.getAllBusinesses().stream().map(b -> convertToDto(b)).collect(Collectors.toList());
-    }
-
-    @GetMapping(value = {"/businesses/{name}", "businesses/{name}/"})
-    public ResponseEntity<?> getBusinessByName(@PathVariable("name") String name) throws IllegalArgumentException {
-        try{
-            Business business = businessService.getBusiness(name);
-            return new ResponseEntity<>(convertToDto(business), HttpStatus.OK);
-        }catch (IllegalArgumentException e){
-            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //CREATE
-    @PostMapping(value = {"/business/create", "/business/create/"})
-    public ResponseEntity<?> createBusiness(@RequestBody BusinessDto businessDto) {
-        try {
-            Business business = businessService.createBusiness(businessDto.getName(), businessDto.getAddress(), businessDto.getPhoneNumber(), businessDto.getEmail(), businessDto.getNumberOfRepairSpots());
-            return new ResponseEntity<>(convertToDto(business), HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //UPDATE
-    @PostMapping(value = {"/business/update", "/business/update/"})
-    public ResponseEntity<?> updateBusiness(@RequestBody BusinessDto businessDto){
-        try{
-            Business business = businessService.updateBusiness(businessDto.getName(), businessDto.getAddress(), businessDto.getPhoneNumber(), businessDto.getEmail(), businessDto.getNumberOfRepairSpots());
-            return new ResponseEntity<>(convertToDto(business), HttpStatus.CREATED);
-        }catch (IllegalArgumentException e){
-            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //TODO: Update List<TimeSlot> vacations?
 
 
     /**
-     * Convert business to DTO
+     * Get request for all businesses
      *
-     * @param b Business
-     * @return businessDto BusinessDTo
+     * @return a list of all businesses
      */
-    private BusinessDto convertToDto(Business b) {
-        if (b == null) {
-            throw new IllegalArgumentException("The business doesn't exist");
+    @GetMapping(value = {"/all", "/all/"})
+    public ResponseEntity<?> getAllBusinesses() {
+        try {
+            List<BusinessDto> businessDtosList = businessService.getAllBusinesses();
+            return new ResponseEntity<>(businessDtosList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
-        //Create businessDto
-        BusinessDto businessDto = new BusinessDto(b.getName(), b.getAddress(), b.getEmail(), b.getPhoneNumber(), b.getNumberOfRepairSpots());
-
-        return businessDto;
     }
+
+    /**
+     * Get a business by name
+     *
+     * @param name unique business name
+     * @return the requested business if found
+     */
+    @GetMapping(value = {"/{name}", "/{name}/"})
+    public ResponseEntity<?> getBusinessByName(@PathVariable("name") String name) {
+        try {
+            BusinessDto businessDto = businessService.getBusinessByName(name);
+            return new ResponseEntity<>(businessDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Create a new business
+     *
+     * @param name                name of the business (String)
+     * @param address             address of the business (String)
+     * @param phoneNumber         phone number of the business (String)
+     * @param email               email of the business (String)
+     * @param numberOfRepairSpots number of repair spots of the business (int)
+     * @return the new business if created successfully
+     */
+    @PostMapping(value = {"/create/{name}", "/create/{name}/"})
+    public ResponseEntity<?> createBusiness(@PathVariable("name") String name, @RequestParam String address,
+                                            @RequestParam String phoneNumber, @RequestParam String email, @RequestParam int numberOfRepairSpots) {
+        try {
+            return new ResponseEntity<>(businessService.createBusiness(name, address, phoneNumber, email, numberOfRepairSpots), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Updates business information
+     *
+     * @param businessDto the update to the existing business information
+     * @return the updated business if updated successfully
+     */
+    @PostMapping(value = {"/update", "/update/"})
+    public ResponseEntity<?> updateBusiness(@RequestBody BusinessDto businessDto) {
+        try {
+            return new ResponseEntity<>(businessService.updateBusiness(businessDto.getName(), businessDto.getAddress(),
+                    businessDto.getPhoneNumber(), businessDto.getEmail(), businessDto.getNumberOfRepairSpots()), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+    /*    //TODO: I assume there's a method to convert TimeSlot -> TimeSlotDto
+     *//**
+     * Get request for all vacations for a business
+     * @return a list of all vacations
+     *//*
+    @GetMapping(value = {"/vacations", "/vacations/"})
+    public ResponseEntity<?> getAllVacations() {
+        try{
+            List<TimeSlotDTO> vacationDtoList = new ArrayList<>();
+
+            //Convert all TimeSlot vacations into TimeSlotDTO
+            for(vacation:businessService.getAllVacations()){
+                vacationList.add(TimeSlotService.timeslotToDTO(vacation));
+            }
+
+            return new ResponseEntity<>(vacationDtoList,HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    } */
 
 
 }
--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/repository/BusinessRepository.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/repository/BusinessRepository.java
@@ -1,7 +1,6 @@
 package ca.mcgill.ecse321.repairshop.repository;
 
 import ca.mcgill.ecse321.repairshop.model.Business;
-import ca.mcgill.ecse321.repairshop.model.TimeSlot;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
