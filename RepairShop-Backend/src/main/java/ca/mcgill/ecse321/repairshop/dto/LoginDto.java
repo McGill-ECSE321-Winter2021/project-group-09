99fb4a1377ece1d1962ce72be0af618f2bb3f4b6
Jacek Dziewonski <jacek@live.ca>
2021-03-09 22:56:22 -0800

    started work on login

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dto/LoginDto.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.repairshop.dto;
 
+import ca.mcgill.ecse321.repairshop.service.utilities.UserType;
 import com.sun.istack.NotNull;
 
 public class LoginDto {
@@ -8,6 +9,21 @@ public class LoginDto {
     private String email;
     @NotNull
     private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    @NotNull
+    private UserType userType;
 
 
     public String getEmail() {
