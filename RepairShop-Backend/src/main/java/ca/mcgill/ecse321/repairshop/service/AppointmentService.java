30486d612d5b8e52628dde459d2b95849afe9411
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-03-10 21:48:14 -0500

    Started to set up isBookable

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/service/AppointmentService.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/service/AppointmentService.java
@@ -4,14 +4,18 @@ import ca.mcgill.ecse321.repairshop.dto.AppointmentDto;
 import ca.mcgill.ecse321.repairshop.dto.ServiceDto;
 import ca.mcgill.ecse321.repairshop.dto.TechnicianDto;
 import ca.mcgill.ecse321.repairshop.dto.TimeSlotDto;
+import ca.mcgill.ecse321.repairshop.model.Appointment;
 import ca.mcgill.ecse321.repairshop.model.Technician;
 import ca.mcgill.ecse321.repairshop.repository.AppointmentRepository;
 import ca.mcgill.ecse321.repairshop.repository.TechnicianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import static ca.mcgill.ecse321.repairshop.service.TechnicianService.technicianToDTO;
+import static ca.mcgill.ecse321.repairshop.service.TimeSlotService.timeslotToDTO;
 
 import javax.transaction.Transactional;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Service
 public class AppointmentService {
@@ -28,6 +32,52 @@ public class AppointmentService {
     @Autowired
     TimeSlotService timeSlotService;
 
+    @Autowired
+    BusinessService businessService;
+
+    @Transactional
+    public boolean isBookable(Timestamp startTime, ServiceDto serviceDto, String technicianEmail) throws Exception {
+
+        // Get timeslot of appointment to be booked
+        TimeSlotDto timeSlotDto = new TimeSlotDto();
+        timeSlotDto.setStartDateTime(startTime);
+        Timestamp endTime = new Timestamp(startTime.getTime() + (long) serviceDto.getDuration() * 30 * 60 * 1000);
+        timeSlotDto.setEndDateTime(endTime);
+
+        // Get Technician
+        Technician technician = technicianRepository.findTechnicianByEmail(technicianEmail);
+        if (technician == null) throw new Exception("The technician's email is invalid");
+
+        // Get Technician's appointments
+        List<Appointment> appointments = technician.getAppointments();
+
+        // Get their corresponding timeslots
+        List<TimeSlotDto> appointmentTimeslots = new ArrayList<>();
+
+        for (Appointment appointment : appointments) {
+            //appointmentTimeslots.add(timeslotToDTO(appointment.getTimeSlot())); TODO: modify timeslots
+        }
+
+        // Get all holidays
+        List<TimeSlotDto> holidayTimeslots = businessService.getAllBusinesses().get(0).getHolidays(); // There will always only be one business
+
+        // check if it can be booked at that time
+
+
+
+
+
+
+        return false;
+    }
+
+    /** Method to book an appointment given a valid timeslot
+     * @param startTimestamp when the appointment will start
+     * @param serviceName the name of the appointment's service
+     * @param technicianEmails the emails of the technicians that can perform the service at the specified start time
+     * @return an AppointmentDto for the bookedAppointment
+     * @throws Exception for invalid timestamp, service name or technician's email
+     */
     @Transactional
     public AppointmentDto bookAppointment(String startTimestamp, String serviceName, String technicianEmails) throws Exception {
 
@@ -74,7 +124,9 @@ public class AppointmentService {
         // (the service duration is the number of blocks of 30 minutes)
         int durationInMillis = serviceDto.getDuration() * 30 * 60 * 1000;
         Timestamp endTime = new Timestamp(startTime.getTime() + durationInMillis);
-        TimeSlotDto timeSlotDto = timeSlotService.createTimeslot(startTime, endTime);
+        TimeSlotDto timeSlotDto = new TimeSlotDto();
+        timeSlotDto.setStartDateTime(startTime);
+        timeSlotDto.setEndDateTime(endTime);
 
         // TODO: create appointment with proper values
         return createAppointment(timeSlotDto, serviceDto, technicianToDTO(technician));
