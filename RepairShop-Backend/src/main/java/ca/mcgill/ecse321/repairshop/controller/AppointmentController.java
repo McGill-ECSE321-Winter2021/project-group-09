934e37f309285f2db5e6ad911de3766b3f966632
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-03-13 10:29:16 -0500

    Modified appointment controller

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/AppointmentController.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/AppointmentController.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.repairshop.controller;
 
 import ca.mcgill.ecse321.repairshop.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +14,22 @@ public class AppointmentController {
     @Autowired
     private AppointmentService appointmentService;
 
-    @PostMapping("/book")
-    public ResponseEntity<?> book(@RequestParam String startTime, @RequestParam String serviceName, @RequestParam String technicianEmails) {
+    @PostMapping("/create")
+    public ResponseEntity<?> createAppointment(@RequestParam String startTimestamp, @RequestParam String serviceName, @RequestParam String customerEmail, @RequestParam String businessName) {
+        try {
+            return new ResponseEntity<>(appointmentService.createAppointment(startTimestamp, serviceName, customerEmail, businessName), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
-
-
-
-        return null; // temp
+    @PostMapping("/possibilities")
+    public ResponseEntity<?> getPossibleAppointments(@RequestParam String startDate, @RequestParam String serviceName, @RequestParam String businessName) {
+        try {
+            return new ResponseEntity<>(appointmentService.getPossibleAppointments(startDate, serviceName, businessName), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
