77ef5de29d100d9e48a5629fc2ff4cfe76053c55
Jacek Dziewonski <46234488+JDziewonski98@users.noreply.github.com>
2021-03-15 11:25:59 -0700

    Authentication (#128)
    * added token authentication
    
    * Finished token implementation with example auth
    
    Co-authored-by: Sandy L <70775435+sandyl289@users.noreply.github.com>

---
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/service/utilities/TokenFilter.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.repairshop.service.utilities;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class TokenFilter extends GenericFilterBean {
+    private final TokenProvider tokenProvider;
+
+    public TokenFilter(TokenProvider j) {
+        tokenProvider = j;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+            throws IOException, ServletException {
+        String token = tokenProvider.resolveToken((HttpServletRequest) req);
+        try {
+            if (token != null && tokenProvider.validateToken(token)) {
+                Authentication auth = tokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        filterChain.doFilter(req, res);
+    }
+}
