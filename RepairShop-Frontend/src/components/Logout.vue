c699d8155c0b0c492d062c4c6e46100580cb60c4
Jacek Dziewonski <46234488+JDziewonski98@users.noreply.github.com>
2021-03-24 22:58:01 -0700

    Frontend login (#180)
    * started login and navbar
    
    * added register tab
    store logged in user state
    conditional rendering on register
    
    * added logout and tweaked other pages

---
+++ RepairShop-Frontend/src/components/Logout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="titleContainer">
+    <h1 v-show="success">Logged Out Successfully!</h1>
+    <h1 v-show="!success">Your were not logged in.</h1>
+  </div>
+</template>
+
+<script>
+import { LOGOUT_ENDPOINT, LOCALHOST_BACKEND } from "../constants/constants";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      success: true
+    };
+  },
+  mounted() {
+    if (this.$root.$data.email === null) {
+      console.log(this.$root.$data.email);
+      //user was not logged in
+      this.success = false;
+      return;
+    }
+    axios
+      .post(LOCALHOST_BACKEND + LOGOUT_ENDPOINT, {
+        email: this.$root.$data.email,
+        password: this.$root.$data.password,
+        userType: this.$root.$data.userType
+      })
+      .then(
+        response => {
+          this.$root.$data.email = null;
+          this.$root.$data.password = null;
+          this.$root.$data.userType = null;
+          this.$root.$data.token = null;
+          alert("Logout Success.");
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }
+};
+</script>
+
+<style>
+#titleContainer {
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
