8438ab6ef00fab20711e9d7fc3d44002ebe94515
Sandy Lao <sandy.lao@mail.mcgill.ca>
2021-03-25 15:44:37 -0400

    Change backendPort to 8080 (Credit to Tyler)

--- RepairShop-Frontend/config/index.js
+++ RepairShop-Frontend/config/index.js
@@ -31,7 +31,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 8089,
     backendHost: '127.0.0.1',
-    backendPort: 8089,
+    backendPort: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
