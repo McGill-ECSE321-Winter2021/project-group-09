b3d07d62ba3a4982ffc757fb0e93d826ad12a1f2
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-03-25 17:17:54 -0400

    Modified navbar for book appointment

--- RepairShop-Frontend/src/components/TopNavbar.vue
+++ RepairShop-Frontend/src/components/TopNavbar.vue
@@ -3,17 +3,20 @@
     <!-- Title button -->
     <b-navbar-brand to="/">Repair Shop</b-navbar-brand>
     <!-- Left aligned buttons -->
-    <b-navbar-nav>
+    <b-navbar-nav v-show="!this.$root.$data.email">
       <b-nav-item to="Login">Log In</b-nav-item>
       <b-nav-item to="Register">Register</b-nav-item>
     </b-navbar-nav>
 
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item v-show="this.$root.$data.email" disabled
-        >Logged in as: {{ this.$root.$data.email }}</b-nav-item
-      >
+    <b-navbar-nav v-show="this.$root.$data.userType == 'Customer'">
+      <b-nav-item to="book">Book Appointment</b-nav-item>
+    </b-navbar-nav>
+
+    <b-navbar-nav class="ml-auto" v-show="this.$root.$data.email">
+      <b-nav-item disabled>Logged in as: {{ this.$root.$data.email }}</b-nav-item>
       <b-nav-item to="Logout">Logout</b-nav-item>
     </b-navbar-nav>
+
   </b-navbar>
 </template>
 
