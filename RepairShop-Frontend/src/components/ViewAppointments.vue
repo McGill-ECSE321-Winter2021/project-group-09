63819b7e836154f4897828cffc69de66799c0708
Jacek Dziewonski <jacek@live.ca>
2021-03-27 00:25:44 -0700

    added date display and cancellation

--- RepairShop-Frontend/src/components/ViewAppointments.vue
+++ RepairShop-Frontend/src/components/ViewAppointments.vue
@@ -9,12 +9,28 @@
           </span>
         </div>
         <div v-else>
-          <b-table :items="items" :fields="fields" :outlined="true">
+          <b-table
+            :items="items"
+            :fields="fields"
+            :outlined="true"
+            :key="this.items.length"
+          >
             <template #cell(cancellation)="row">
               <b-button
                 size="sm"
                 v-on:click="cancelAppointment(row.item.ID)"
                 class="mr-2"
+                variant="danger"
+                v-if="checkIfWeekAhead(row.item.ID)"
+              >
+                Cancel Appointment
+              </b-button>
+              <b-button
+                size="sm"
+                v-on:click="cancelAppointment(row.item.ID)"
+                class="mr-2"
+                disabled
+                v-else
               >
                 Cancel Appointment
               </b-button>
@@ -40,38 +56,19 @@ export default {
       errorViewServices: "",
       appointments: [],
       fields: ["ID", "Service", "start", "end", "cancellation"],
-      items: []
+      items: [],
+      idToDateTimeMap: {}
     };
   },
 
+  //fetch all of this customer's appointments and display them in a table
   created: function() {
-    AXIOS.get("/api/customer/" + this.$root.$data.email + "/appointments", {
-      headers: {
-        token: this.$root.$data.token
-      }
-    })
-      .then(response => {
-        this.appointments = response.data;
-
-        this.appointments.forEach(item => {
-          this.items.push({
-            ID: item.appointmentID,
-            Service: item.serviceDto.name,
-            start: this.displayDateTime(item.timeSlotDto.startDateTime),
-            end: this.displayDateTime(item.timeSlotDto.endDateTime)
-          });
-        });
-        console.log(this.items);
-      })
-      .catch(e => {
-        console.log(e);
-      });
+    this.getAppointments();
   },
   methods: {
+    // Should output something like "Tue Mar 02 2021 10:00:00 GMT-0500 (Eastern Standard Time)" given a timestamp
     displayDateTime(dateTime) {
-      console.log("here");
-      let date = new Date(dateTime).toString(); // Should output something like "Tue Mar 02 2021 10:00:00 GMT-0500 (Eastern Standard Time)"
-      console.log("there");
+      let date = new Date(dateTime).toString();
       if (date == "Invalid Date") return "";
       else
         return (
@@ -82,14 +79,16 @@ export default {
           date.slice(16, 21)
         );
     },
+    //cancels appointment by id if it is valid, forcing a component refresh once list is updated
     cancelAppointment(id) {
-      console.log(id);
       AXIOS.delete(CANCEL_APPOINTMENT_ENDPOINT + id, {
         headers: {
           token: this.$root.$data.token
         }
       })
         .then(response => {
+          this.items = [];
+          this.getAppointments();
           console.log(response);
         })
         .catch(error => {
@@ -103,6 +102,38 @@ export default {
               alert("Something went wrong.");
             }
           }
+        });
+    },
+    //checks if an appointment (by id) is at least a week away from today
+    checkIfWeekAhead(id) {
+      let target = new Date(this.idToDateTimeMap[id]);
+      target.setDate(target.getDate() - 7);
+      if (target > new Date()) return true;
+      return false;
+    },
+    //fetches all of customer's appointments
+    getAppointments() {
+      AXIOS.get("/api/customer/" + this.$root.$data.email + "/appointments", {
+        headers: {
+          token: this.$root.$data.token
+        }
+      })
+        .then(response => {
+          this.appointments = response.data;
+
+          this.appointments.forEach(item => {
+            this.items.push({
+              ID: item.appointmentID,
+              Service: item.serviceDto.name,
+              start: this.displayDateTime(item.timeSlotDto.startDateTime),
+              end: this.displayDateTime(item.timeSlotDto.endDateTime)
+            });
+            this.idToDateTimeMap[item.appointmentID] =
+              item.timeSlotDto.startDateTime;
+          });
+        })
+        .catch(e => {
+          console.log(e);
         });
     }
   }
