91234c6469c5d072a6c62582f7c9e073c89998f0
Sandy Lao <sandy.lao@mail.mcgill.ca>
2021-04-02 13:19:10 -0400

    Welcome message
    - If no repair shop name yet: "Welcome"
    - There's a business already: "Welcome to <businessName>"

--- RepairShop-Frontend/src/components/Hello.vue
+++ RepairShop-Frontend/src/components/Hello.vue
@@ -1,33 +1,37 @@
 <template>
   <div v-if="errorGetBusiness">
-            <span v-if="errorGetBusiness" style="color: red">
-              {{ errorGetBusiness }}
-            </span>
+    <span v-if="errorGetBusiness" style="color: red">
+      {{ errorGetBusiness }}
+    </span>
   </div>
-<div v-else>
-  <h1>Welcome to {{name}}!</h1>
-</div>
 
+  <div v-else>
+    
+    <h1>Welcome!</h1>
+
+    <div v-if="name">
+      <h1>Welcome to {{ name }}!</h1>
+    </div>
+
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import {
-  GET_BUSINESS_ENDPOINT
-} from "../constants/constants";
+import { GET_BUSINESS_ENDPOINT } from "../constants/constants";
 var config = require("../../config");
 var AXIOS = axios.create({
   baseURL: "http://" + config.dev.backendHost + ":" + config.dev.backendPort,
 });
 export default {
-  data(){
-    return{
-        name: "",
-        errorGetBusiness: ""
-    }
+  data() {
+    return {
+      name: "",
+      errorGetBusiness: "",
+    };
   },
-created: function(){
-   AXIOS.get(GET_BUSINESS_ENDPOINT)
+  created: function () {
+    AXIOS.get(GET_BUSINESS_ENDPOINT)
       .then((response) => {
         this.name = response.data.name;
       })
@@ -35,9 +39,7 @@ created: function(){
         if (e.response.status == 404) this.errorGetBusiness = e.response.data;
         else this.errorGetBusiness = e;
       });
-}
-
-
+  },
 };
 </script>
 
