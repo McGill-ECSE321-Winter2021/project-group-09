fcec8f1d44fc938ea5ab0971f59cc09b9adda09c
Shaswata Bhattacharyya <shaswatabhatt2030@gmail.com>
2021-04-08 22:48:40 +0600

    Completed registration page. Page not working for some reason.

--- RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/MainActivity.java
+++ RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/MainActivity.java
@@ -12,8 +12,10 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,4 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
---
+++ RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/Register.java
@@ -0,0 +1,114 @@
+package ca.mcgill.ecse321.repairshop;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class Register extends AppCompatActivity {
+    private Button registerButton;
+    private String error = null;
+    private String success = null;
+    private String name = null;
+    private String email = null;
+    private String phoneNumber = null;
+    private String address = null;
+    private String password = null;
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.signUpError);
+        tvError.setText(error);
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void refreshSuccessMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.signUpSuccess);
+        tvError.setText(success);
+        if (success == null || success.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        //set the onClick methods for the Sign Up button
+        registerButton = findViewById(R.id.signUpButton);
+        registerButton.setOnClickListener(this::signUp);
+
+        refreshErrorMessage();
+    }
+
+
+    public void signUp(View v) {
+        System.out.println("Here");
+        error = "";
+
+        TextView customerName = (TextView)findViewById(R.id.customerNameRegister);
+        name = String.valueOf(customerName.getText());
+        TextView customerAddress = (TextView)findViewById(R.id.customerAddressRegister);
+        address = String.valueOf(customerAddress.getText());
+        TextView customerPassword = (TextView)findViewById(R.id.customerPasswordRegister);
+        password = String.valueOf(customerPassword.getText());
+        TextView customerPhone = (TextView)findViewById(R.id.customerPhoneRegister);
+        phoneNumber = String.valueOf(customerPhone.getText());
+        TextView customerEmail = (TextView)findViewById(R.id.customerEmailRegister);
+        email = String.valueOf(customerEmail.getText());
+
+
+        RequestParams params = new RequestParams();
+        params.add("name", name);
+        params.add("email", email);
+        params.add("password", password);
+        params.add("phoneNumber", phoneNumber);
+        params.add("address", address);
+
+        HttpUtils.post("/api/customer/register", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                success = "Account Created";
+                refreshSuccessMessage();
+                customerName.setText("");
+                customerEmail.setText("");
+                customerPhone.setText("");
+                customerAddress.setText("");
+                customerPassword.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                    System.out.println(error);
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+}
