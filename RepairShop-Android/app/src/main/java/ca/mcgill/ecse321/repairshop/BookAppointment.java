b214354502177cf092ac9b2092809dfe2a4ca304
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-04-10 14:29:10 -0400

    Initial version of part 2 done. Failing at request

--- RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/BookAppointment.java
+++ RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/BookAppointment.java
@@ -22,8 +22,9 @@ public class BookAppointment extends BaseActivity {
 
     String errorText = "";
 
-    JSONArray services;
-    JSONObject service;
+    JSONObject service, timeSlot;
+
+    String targetDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,11 @@ public class BookAppointment extends BaseActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
 
-                services = response;
+                if (response.length() == 0) {
+                    errorText = "There are currently no services";
+                    refreshError();
+                    return;
+                }
 
                 ArrayList<String> displayServices = new ArrayList<>();
 
@@ -46,7 +51,7 @@ public class BookAppointment extends BaseActivity {
                         String currName = currService.getString("name");
                         String currDuration = displayDuration(currService.getInt("duration"));
                         double currPrice = Math.round(currService.getDouble("price") * 100.0) / 100.0;
-                        displayServices.add(currName + ", for " + currDuration + " $(" + currPrice + ")");
+                        displayServices.add(currName + ", for " + currDuration + " ($" + currPrice + ")");
                     } catch (JSONException e) {
                         errorText = e.getMessage();
                         refreshError();
@@ -60,7 +65,11 @@ public class BookAppointment extends BaseActivity {
                 servicesListView.setAdapter(servicesArrayAdapter);
                 servicesListView.setOnItemClickListener((adapterView, view, i, l) -> {
                     try {
-                        service = services.getJSONObject(i);
+                        service = response.getJSONObject(i);
+
+                        // Show all available booking times
+                        toPart2();
+
                     } catch (JSONException e) {
                         errorText = e.getMessage();
                         refreshError();
@@ -82,7 +91,75 @@ public class BookAppointment extends BaseActivity {
         });
 
 
+    }
 
+    // Set up the next part for booking - display all available appointment times
+    private void toPart2() throws JSONException {
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.add("startDate", targetDate);
+        requestParams.add("serviceName", service.getString("name"));
+
+        // Get possible appointment times
+        HttpUtils.get("api/appointment/possibilities", requestParams, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+
+                findViewById(R.id.book1).setVisibility(View.GONE);
+                findViewById(R.id.book2).setVisibility(View.VISIBLE);
+
+                if (response.length() == 0) {
+                    errorText = "There are currently no available time slots";
+                    refreshError();
+                    return;
+                }
+
+                ArrayList<String> displayPossibilities = new ArrayList<>();
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject currTimeSlot = response.getJSONObject(i);
+                        String currStart = displayDateTime(currTimeSlot.getString("startDateTime"));
+                        String currEnd = displayDateTime(currTimeSlot.getString("endDateTime"));
+                        displayPossibilities.add(currStart + " to " + currEnd);
+                    } catch (JSONException e) {
+                        errorText = e.getMessage();
+                        refreshError();
+                        return;
+                    }
+                }
+
+                ListView timeSlotsListView = findViewById(R.id.possibleTimeSlotsList);
+                ArrayAdapter<String> timeSlotsArrayAdapter = new ArrayAdapter<>(BookAppointment.this, android.R.layout.simple_list_item_1, displayPossibilities);
+
+                timeSlotsListView.setAdapter(timeSlotsArrayAdapter);
+                timeSlotsListView.setOnItemClickListener((adapterView, view, i, l) -> {
+                    try {
+                        timeSlot = response.getJSONObject(i);
+
+                        // Show all available booking times
+//                        toPart2();
+
+                    } catch (JSONException e) {
+                        errorText = e.getMessage();
+                        refreshError();
+                    }
+                    Log.v("time slot", timeSlot.toString());
+                });
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    errorText = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    errorText = e.getMessage();
+                }
+                refreshError();
+            }
+
+        });
 
     }
 
@@ -97,6 +174,12 @@ public class BookAppointment extends BaseActivity {
     private String displayDuration(int duration) {
         if (duration == 2) return "1 hour";
         else return (duration / 2.0) + " hours";
+    }
+
+    // Helper to convert a timestamp format (2021-03-02T15:00:00.000+00:00) to format Tue Mar 02, 2021 at HH:mm (in local timezone)
+    private String displayDateTime(String dateTime) {
+        Log.v("date time", dateTime);
+        return dateTime;
     }
 
 }
