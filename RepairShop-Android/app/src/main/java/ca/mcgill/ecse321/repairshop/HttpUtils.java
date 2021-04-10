282eb05d837a258aaa4c425bc55a14c8362610f1
T-Syme <70815823+T-Syme@users.noreply.github.com>
2021-04-10 18:36:54 -0400

    Added Jacek's login stuff

--- RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/HttpUtils.java
+++ RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/HttpUtils.java
@@ -1,8 +1,16 @@
 package ca.mcgill.ecse321.repairshop;
 
+import android.content.Context;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class HttpUtils {
 
@@ -11,7 +19,7 @@ public class HttpUtils {
     public static final String DEFAULT_BASE_URL = "https://repairshop-backend-ecse321-09.herokuapp.com/";
 
     private static String baseUrl;
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static final AsyncHttpClient client = new AsyncHttpClient();
 
     static {
         baseUrl = DEFAULT_BASE_URL;
@@ -31,6 +39,11 @@ public class HttpUtils {
 
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void postWithBody(Context context, String url, JSONObject body, AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
+        StringEntity stringEntity = new StringEntity(body.toString());
+        client.post(context, getAbsoluteUrl(url), stringEntity, "application/json", responseHandler);
     }
 
     public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
---
+++ RepairShop-Android/app/src/main/java/ca/mcgill/ecse321/repairshop/State.java
@@ -0,0 +1,7 @@
+package ca.mcgill.ecse321.repairshop;
+
+import android.app.Application;
+
+public class State extends Application {
+    public String token;
+}
